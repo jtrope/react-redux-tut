fab54fe7bb0019bd1afde4d4430735b3aab2401d
Jonathan Trope <jonathan@jonathantrope.com>
2019-06-27 06:26:03 -0400

    gunit

--- components/src/index.js
+++ components/src/index.js
@@ -6,11 +6,9 @@ import CommentDetail from './CommentDetail'
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <CommentDetail author="Sam" timeAgo="Today at 4:45pm" text="Nice blog post!" avatarSrc={faker.image.avatar()} />
+            <CommentDetail author="Alex" timeAgo="Today at 2:00am" text="I like chicken" avatarSrc={faker.image.avatar()} />
+            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00pm" text="I like liver" avatarSrc={faker.image.avatar()} />
         </div>
     );
 };
