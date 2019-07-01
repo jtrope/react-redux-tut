9650c415c97db5954a7d5b28c9f0a0d5f9141a65
Jonathan Trope <jonathan@jonathantrope.com>
2019-07-01 07:16:58 -0400

    stable

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class App extends React.Component {
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
+    constructor(props) {
+        super(props);
+        this.state = { lat: null , errorMessage: ''};
 
-    return <div>Latitude: </div>
-};
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => this.setState({lat: position.coords.latitude}),
+            (err) => this.setState({errorMessage: err.message})
+        );
+    };
+
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        return <div>Loading!</div>
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
