31d07de372cb233bc0efbe76ee35a55c88f47984
Jonathan Trope <jonathan@jonathantrope.com>
2019-07-10 07:55:05 -0400

    Mad user value yo

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+
+    onInputClick() {
+        console.log('Input was clicked');
+    }
+
+    render() {
+        return (
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <input type="text" onChange={this.onInputChange} onClick={this.onInputClick} />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
